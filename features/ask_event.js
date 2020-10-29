6957e8bfc47ee9980b9219408034fcee21a1dcd5
htchan <jacky19980619@gmail.com>
2020-10-30 02:04:23 +0800

    implement basic ask event features and related api
    - randomly suggest one event only
    - funther improvement
    	- ask user preference (hike, shopping, museum) before

--- features/ask_event.js
+++ features/ask_event.js
@@ -1,5 +1,26 @@
 const { BotkitConversation } = require("botkit");
 
 module.exports = function(controller) {
-    
+    let timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+    let apiHost = 'localhost:3000';
+
+    let callEventApi = async() => {
+        return await fetch(`http://${apiHost}/api/event.json`)
+        .then( response => response.json() )
+        .then( data => data.filter( item => item.available ) )
+    }
+
+    //TODO: ask user what type of event they want
+
+    controller.hears(["event", 'activities', 'activity'],
+        'message', async (bot, message) => {
+            await bot.reply(message, 'Hey, are you looking for some interesting events?');
+            await timeout(200);
+            let events = await callEventApi();
+            let event = events[Math.floor(Math.random(events.length))];
+            await bot.reply(message, `You may be interested in ${event.title}`)
+            await bot.reply(message, event.desc);
+            await bot.reply(message, `Here is the event details:<br/>Title: ${event.title}<br/>time: ${event.time}<br/>Location: ${event.location}<br/>Link: <a href="${event.url}" target="_blank">${event.url}</a>`)
+        });
 }
